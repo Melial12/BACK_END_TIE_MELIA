59ddc6cc8d1d80847f69095f2c18b719e53a5737
melia <meliamadzrongatul12345@gmail.com>
2023-10-26 19:58:23 +0700

    API

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,13 +1,19 @@
 <?php
+
 use CodeIgniter\Router\RouteCollection;
+
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('insertproducts', 'ProductController::insertProduct');
-$routes->post('insertproducts', 'ProductController::insertProduct');
+$routes->get('insertproduct', 'ProductController::insertProduct');
 $routes->get('products', 'ProductController::readProduct');
 $routes->get('edit-product/(:any)', 'ProductController::getProduct/$1');
 $routes->post('update-product/(:any)', 'ProductController::updateProduct/$1');
 $routes->get('delete-product/(:any)', 'ProductController::deleteProduct/$1');
-//
+
+
+$routes->group('api', function ($routes){
+    $routes->get('products', 'ProductController::readProductApi');
+    $routes->get('product/(:any)', 'ProductController::getProductApi/$1');
+});
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -2,70 +2,86 @@
 namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\ProductModel;
+use Codeigniter\API\ResponseTrait;
+
 
 class ProductController extends BaseController {
-
-    private $product;
+    use ResponseTrait;
 
     public function __construct(){
         $this->product = new ProductModel();
     }
-
     public function insertProduct(){
-        if ($this->request->getMethod() === 'post') {
-            $nama_product = $this->request->getVar("nama_product");
-            $description = $this->request->getVar("description");
-    
-            // Lakukan validasi atau operasi lain sesuai kebutuhan
-    
-            // Setelah validasi atau operasi lainnya, Anda dapat menyimpan data ke database
-            $data = [
-                // 'nama_product' => 'Buku',
-                // 'description' => 'Jendela Dunia'
-                'nama_product' => $nama_product,
-                'description' => $description
-            ];
-    
-            $this->product->insertProductORM($data);
-    
-            // Redirect ke halaman lain atau tampilkan pesan sukses
-            return redirect()->to(base_url("products"));
-        } else {
-            // Jika bukan metode POST, tampilkan formulir
-            return view('insert_product');
-        }
+        $data = [
+            'nama_product' => 'Es Cendol',
+            'description' => 'Minuman',
+        ];
+
+        $this->product->insertProductORM($data);
     }
-    
-    
+
     public function readProduct(){
         $products = $this->product->findAll();
         $data = [
-            'data' => $products
+            'products' => $products
         ];
-
         return view('product', $data);
     }
-        public function getProduct($id){
-            $product = $this->product->where('id', $id)->first();
-            $data = [
-                'product' => $product
-                ];
-            return view('edit_product', $data);
-        }
-        public function updateProduct($id){
-            $nama_product = $this->request->getVar("nama_product");
-            $description = $this->request->getVar("description");
-            $data = [
-                'nama_product' => $nama_product,
-                'description' => $description
-            ];
-            $this->product->update($id, $data);
-            return redirect()->to(base_url("products"));
-        }
-        public function deleteProduct($id){
-            $this->product->delete($id);
-            return redirect()->to(base_url("products"));
-        }
-    
+
+    public function readProductApi(){
+        $products = $this->product->findAll();
+
+        return $this->respond([
+            'code' => 200,
+            'status' => 'OK',
+            'data' => $products
+        ]);
     }
-    //
+
+    public function getProduct($id){
+        $product = $this->product->where('id',$id)->first();
+        $data = [
+            'product' => $product
+        ];
+        return view('edit_product', $data);
+    }
+
+    public function getProductApi($id){
+        $product = $this->product->where('id',$id)->first();
+        
+        if (!$product){
+            $this->response->setStatusCode(404);
+            return $this->response->setJSON(
+                [
+                    'code' => 404,
+                    'status' => 'NOT FOUND',
+                    'data' => 'product not found'
+                ]
+                );
+        }
+        
+        return $this->respond([
+            'code' => 200,
+            'status' => 'OK',
+            'data' => $product
+        ]);
+
+        return view('edit_product', $data);
+    }
+    public function updateProduct($id){
+        $nama_product = $this->request->getVar('nama_product');
+        $description = $this->request->getVar('description');
+
+        $data = [
+            'nama_product' => $nama_product,
+            'description' => $description
+        ];
+        $this->product->update($id, $data);
+        return redirect()->to(base_url('products'));
+    }
+
+    public function deleteProduct($id){
+        $this->product->delete($id);
+        return redirect()->to(base_url('products'));
+    }
+}
